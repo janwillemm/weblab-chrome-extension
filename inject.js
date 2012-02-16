8fc36d3bf99834b1a089da7602f474969deaa55b
Jan-Willem Manenschijn <jw.moonshine@gmail.com>
2012-02-16 14:56:27 +0100

    Verbeteringen :)

--- inject.js
+++ inject.js
@@ -1,7 +1,4 @@
-var sheet = document.createElement('style')
-sheet.innerHTML = "div.question {height:350px !important; overflow:auto;}";
-document.body.appendChild(sheet);
-
+//alert("Injected javascript successfully!");
 window.addEventListener("keydown", function(event) {
   // Bind to both command (for Mac) and control (for Win/Linux)
   var modifier = event.ctrlKey || event.metaKey;
@@ -14,24 +11,24 @@ window.addEventListener("keydown", function(event) {
   // press ctrl + s to save and compile!
   if (modifier && event.keyCode == 83) {
 	  event.preventDefault();
-	  var element = document.getElementById("save");
+      if(shiftKey)
+        var element = document.getElementById("submit");
+      else
+	      var element = document.getElementById("save");
 	  element.dispatchEvent(e);
   }
   // press alt + r to test your code.
-  else if((altKey && event.keyCode == 82) || (modifier && event.keyCode == 82){
+  else if((altKey && event.keyCode == 82) || (modifier && event.keyCode == 82)){
   	event.preventDefault();
   	var element = document.getElementById("compilerOutput").firstChild;
   	testElement = (shiftKey) ? element.nextSibling.nextSibling : element;
-  	console.log(element);
-  	console.log(testElement);
     testElement.dispatchEvent(e);
   }
 
 }, false);
 
-window.addEventListener("resize", function(event){
-	console.log(parseInt(window.innerHeight));
-	window.innerHeight; //316
-	sheet.innerHTML = "div.question {height:"+(parseInt(window.innerHeight)-356)+"px !important; overflow:auto;";
-
-})
+window.fCopyToClipboard = function(rSource){
+  rSource.select()
+  if(window.clipboardData){ var r=clipboardData.setData('Text',rSource.value); return 1; }
+  else return 0
+}
